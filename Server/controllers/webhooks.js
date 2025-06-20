e3b46237343917696e38e2bfe493dcb356bb7dfb
Sidharthavyas <vsidhartha71@gmail.com>
2025-06-20 16:23:18 +0530

    stripe error resolved

--- Server/controllers/webhooks.js
+++ Server/controllers/webhooks.js
@@ -56,7 +56,7 @@ import Course from "../models/Course.js";
 const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 
-export const stripeWebhooks = async(req,res)=>{
+export const stripeWebhooks = async(request,response)=>{
      const sig = request.headers['stripe-signature'];
 
   let event;
